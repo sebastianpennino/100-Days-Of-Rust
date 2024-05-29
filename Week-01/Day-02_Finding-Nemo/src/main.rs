6388f46ce29f38de17646ff8578cef64e45be15e
sebastianpennino <sebastian@fullbeaker.com>
2024-05-29 03:28:12 -0300

    Day 02 Project completed

--- Week-01/Day-02_Finding-Nemo/src/main.rs
+++ Week-01/Day-02_Finding-Nemo/src/main.rs
@@ -1,14 +1,15 @@
 use std::io;
+use regex::Regex; // https://crates.io/crates/regex
 
 fn find_nemo(str: &str) {
-    let n = "Nemo";
+    let re = Regex::new(r"\bNemo\b").unwrap(); // \b is a word boundary
 
-    match str.find(n) {
-        Some(num) => {
-            println!("Result -> I found Nemo at {}!", num);
+    match re.find(str) {
+        Some(m) => {
+            println!("I found Nemo at {}!", m.start());
         }
         None => {
-            println!("Result -> I can't find Nemo :(");
+            println!("I can't find Nemo :(");
         }
     };
 }
@@ -16,12 +17,12 @@ fn find_nemo(str: &str) {
 fn main() {
     println!("### Lets try to find nemo! ###");
     let mut phrase = String::new();
-    let mut num: u8 = 1;
+    let mut num: u8 = 1; // max 255
 
     loop {
         println!("");
         println!(
-            "[loop: {}] [Please input a phrase on the screen or either write quit/exit to break the loop]", num);
+            "[loop: {}] Please input a phrase or either write quit/exit to exit", num);
         phrase.clear(); // Clear the string buffer for new input
 
         io::stdin()
@@ -30,14 +31,15 @@ fn main() {
 
         // Trim the input to remove any trailing newline characters
         let trimmed_phrase = phrase.trim();
-
+        
+        // Exit by text or last loop
         if trimmed_phrase == "quit" || trimmed_phrase == "exit" || num == 255 {
-            println!("Result -> OK, exiting!");
+            println!("-> OK, exiting!");
             break; // Exit this loop
         }
 
         find_nemo(trimmed_phrase);
 
-        num = num + 1;
+        num = num + 1; // increase the loop count
     }
 }

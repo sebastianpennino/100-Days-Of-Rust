6222cf9e88e04a7d7e2d7b4ad4b13b3837369210
sebastianpennino <sebastian.pennino@gmail.com>
2024-06-19 17:54:08 -0300

    Day 08 - Reducing a few hiccups by using best types

--- Week-02/Day-08_Letter-Combinations-Of-A-Phone-Number/src/lib.rs
+++ Week-02/Day-08_Letter-Combinations-Of-A-Phone-Number/src/lib.rs
@@ -1,29 +1,28 @@
 use std::collections::HashMap;
 
 // generates a hashmap with the rules from the picture
-pub fn generate_key_pad() -> HashMap<i32, Vec<String>> {
-    let mut phone_keypad: HashMap<i32, Vec<String>> = HashMap::new();
+pub fn generate_key_pad() -> HashMap<u8, Vec<char>> {
+    let mut phone_keypad: HashMap<u8, Vec<char>> = HashMap::new();
 
-    // phone_keypad.insert(1, vec![]);
-    phone_keypad.insert(2, vec!["a".to_string(), "b".to_string(), "c".to_string()]);
-    phone_keypad.insert(3, vec!["d".to_string(), "e".to_string(), "f".to_string()]);
-    phone_keypad.insert(4, vec!["g".to_string(), "h".to_string(), "i".to_string()]);
-    phone_keypad.insert(5, vec!["j".to_string(), "k".to_string(), "l".to_string()]);
-    phone_keypad.insert(6, vec!["m".to_string(), "n".to_string(), "o".to_string()]);
-    phone_keypad.insert(7, vec!["p".to_string(), "q".to_string(), "r".to_string(), "s".to_string()]);
-    phone_keypad.insert(8, vec!["t".to_string(), "u".to_string(), "v".to_string()]);
-    phone_keypad.insert(9, vec!["w".to_string(), "x".to_string(), "y".to_string(), "z".to_string()]);
+    phone_keypad.insert(2, vec!['a', 'b', 'c']);
+    phone_keypad.insert(3, vec!['d', 'e', 'f']);
+    phone_keypad.insert(4, vec!['g', 'h', 'i']);
+    phone_keypad.insert(5, vec!['j', 'k', 'l']);
+    phone_keypad.insert(6, vec!['m', 'n', 'o']);
+    phone_keypad.insert(7, vec!['p', 'q', 'r', 's']);
+    phone_keypad.insert(8, vec!['t', 'u', 'v']);
+    phone_keypad.insert(9, vec!['w', 'x', 'y', 'z']);
 
     return phone_keypad;
 }
 
 // ie "123" -> [1, 2, 3]
-pub fn str_to_digits(str: &str) -> Vec<i32> {
-    let digits: Vec<i32> = str
+pub fn str_to_digits(str: &str) -> Vec<u8> {
+    let digits: Vec<u8> = str
         .chars() // Get all the individual chars in the string
         .filter_map(|c| {
-            return c.to_digit(10).map(|d| d as i32);
-        }) // Cast to i32 (unwrap Option and convert from u32)
+            return c.to_digit(10).map(|d| d as u8);
+        }) // cast to u8
         .collect();
     return digits;
 }
@@ -50,13 +49,13 @@ pub fn str_to_digits(str: &str) -> Vec<i32> {
 //
 //     return result;
 // }
-pub fn generate_permutations(vec_of_letters: Vec<Vec<&str>>) -> Vec<String> {
+pub fn generate_permutations(vec_of_letters: Vec<&Vec<char>>) -> Vec<String> {
     let mut result = vec![String::from("")]; // Start with a vector containing an empty string
 
     for group in vec_of_letters {
         let mut new_result: Vec<String> = Vec::new();
         for prefix in &result {
-            for letter in &group {
+            for letter in group {
                 let new_word = format!("{}{}", prefix, letter);
                 new_result.push(new_word);
             }
@@ -67,54 +66,26 @@ pub fn generate_permutations(vec_of_letters: Vec<Vec<&str>>) -> Vec<String> {
     return result;
 }
 
-pub fn vec_to_letters(digits_vec: Vec<i32>) -> Vec<String> {
+pub fn vec_to_letters(digits_vec: Vec<u8>) -> Vec<String> {
     let keypad = generate_key_pad();
 
-    let mut found_letters: Vec<Vec<&str>> = [].to_vec();
+    let mut found_letters: Vec<&Vec<char>> = [].to_vec();
 
     for digit in digits_vec {
         if keypad.contains_key(&digit) {
-            let unwrap = keypad.get(&digit).unwrap();
-            // TODO: see if we can find a better method (converting Vec<String> to Vec<str>)
-            let str: Vec<&str> = unwrap.iter().map(AsRef::as_ref).collect();
-            found_letters.push(str);
+            let unwrap: &Vec<char> = keypad.get(&digit).unwrap();
+            found_letters.push(unwrap);
         }
     }
 
     let rst = generate_permutations(found_letters);
 
     return rst;
-
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn keypad_generation() {
-        let keypad = generate_key_pad();
-
-        // Check the number of keys in the keypad
-        assert_eq!(keypad.len(), 8);
-
-        // Check specific mappings
-        // assert_eq!(keypad.get(&1), Some(&vec![]));
-        assert_eq!(keypad.get(&2), Some(&vec!["a".to_string(), "b".to_string(), "c".to_string()]));
-        assert_eq!(keypad.get(&3), Some(&vec!["d".to_string(), "e".to_string(), "f".to_string()]));
-        assert_eq!(keypad.get(&4), Some(&vec!["g".to_string(), "h".to_string(), "i".to_string()]));
-        assert_eq!(keypad.get(&5), Some(&vec!["j".to_string(), "k".to_string(), "l".to_string()]));
-        assert_eq!(keypad.get(&6), Some(&vec!["m".to_string(), "n".to_string(), "o".to_string()]));
-        assert_eq!(
-            keypad.get(&7),
-            Some(&vec!["p".to_string(), "q".to_string(), "r".to_string(), "s".to_string()])
-        );
-        assert_eq!(keypad.get(&8), Some(&vec!["t".to_string(), "u".to_string(), "v".to_string()]));
-        assert_eq!(
-            keypad.get(&9),
-            Some(&vec!["w".to_string(), "x".to_string(), "y".to_string(), "z".to_string()])
-        );
-    }
 
     #[test]
     fn single_number_seven() {
@@ -123,20 +94,9 @@ mod tests {
         let digits_vec = str_to_digits(digits_str);
         assert_eq!(digits_vec, expected_vec);
         let permutations = vec_to_letters(digits_vec);
-        let expected_permutations = vec!["p", "q", "r", "s"];
+        let expected_permutations: Vec<String> = vec!["p".to_string(), "q".to_string(), "r".to_string(), "s".to_string()];
         assert_eq!(permutations, expected_permutations);
     }
-
-    // #[test]
-    // fn single_number_weird() {
-    //     let digits_str = "1";
-    //     let expected_vec = vec![1];
-    //     let digits_vec = str_to_digits(digits_str);
-    //     assert_eq!(digits_vec, expected_vec);
-    //     let permutations = vec_to_letters(digits_vec);
-    //     let expected_permutations: Vec<&str> = vec![];
-    //     assert_eq!(permutations, expected_permutations);
-    // }
 
     #[test]
     fn two_numbers() {

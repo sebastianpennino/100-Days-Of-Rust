e58ef6e66540d457c6cd63bdb684a189c97df495
sebastianpennino <sebastian.pennino@gmail.com>
2024-06-20 16:51:30 -0300

    Day 09 - v1 total rework

--- Week-02/Day-09_Trapping-Rain-Water/src/lib.rs
+++ Week-02/Day-09_Trapping-Rain-Water/src/lib.rs
@@ -13,43 +13,61 @@ mod tests {
 
     #[test]
     fn water_1() {
-        let height_map: Vec<i16> = vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1, 0];
-        let output = trap_water_v2(height_map);
-        assert_eq!(output, 6);
+        let height_map_v1: Vec<i16> = vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1, 0];
+        let height_map_v2: Vec<i16> = vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1, 0];
+        let output_v1 = trap_water_v1(height_map_v1);
+        let output_v2 = trap_water_v2(height_map_v2);
+        assert_eq!(output_v1, 6);
+        assert_eq!(output_v2, 6);
     }
 
     #[test]
     fn water_2() {
-        let height_map: Vec<i16> = vec![4, 2, 0, 3, 2, 5];
-        let output = trap_water_v2(height_map);
-        assert_eq!(output, 9);
+        let height_map_v1: Vec<i16> = vec![4, 2, 0, 3, 2, 5];
+        let height_map_v2: Vec<i16> = vec![4, 2, 0, 3, 2, 5];
+        let output_v1 = trap_water_v1(height_map_v1);
+        let output_v2 = trap_water_v2(height_map_v2);
+        assert_eq!(output_v1, 9);
+        assert_eq!(output_v2, 9);
     }
 
     #[test]
     fn water_3() {
-        let height_map: Vec<i16> = vec![0, 0, 4, 0, 0, 5, 0, 5];
-        let output = trap_water_v2(height_map);
-        assert_eq!(output, 13);
+        let height_map_v1: Vec<i16> = vec![0, 0, 4, 0, 0, 5, 0, 5];
+        let height_map_v2: Vec<i16> = vec![0, 0, 4, 0, 0, 5, 0, 5];
+        let output_v1 = trap_water_v1(height_map_v1);
+        let output_v2 = trap_water_v2(height_map_v2);
+        assert_eq!(output_v1, 13);
+        assert_eq!(output_v2, 13);
     }
 
     #[test]
     fn water_4() {
-        let height_map: Vec<i16> = vec![0, 0, 4, 0, 0, 5, 0, 1, 0];
-        let output = trap_water_v2(height_map);
-        assert_eq!(output, 9);
+        let height_map_v1: Vec<i16> = vec![0, 0, 4, 0, 0, 5, 0, 1, 0];
+        let height_map_v2: Vec<i16> = vec![0, 0, 4, 0, 0, 5, 0, 1, 0];
+        let output_v1 = trap_water_v1(height_map_v1);
+        let output_v2 = trap_water_v2(height_map_v2);
+        assert_eq!(output_v1, 9);
+        assert_eq!(output_v2, 9);
     }
 
     #[test]
     fn water_5() {
-        let height_map: Vec<i16> = vec![0, 4, 3, 0, 1, 0, 3, 0, 1, 0];
-        let output = trap_water_v2(height_map);
-        assert_eq!(output, 9);
+        let height_map_v1: Vec<i16> = vec![0, 4, 3, 0, 1, 0, 3, 0, 1, 0];
+        let height_map_v2: Vec<i16> = vec![0, 4, 3, 0, 1, 0, 3, 0, 1, 0];
+        let output_v1 = trap_water_v1(height_map_v1);
+        let output_v2 = trap_water_v2(height_map_v2);
+        assert_eq!(output_v1, 9);
+        assert_eq!(output_v2, 9);
     }
 
     #[test]
     fn water_6() {
-        let height_map: Vec<i16> = vec![0, 4, 3, 0, 1, 0, 2, 0, 1, 0];
-        let output = trap_water_v2(height_map);
-        assert_eq!(output, 6);
+        let height_map_v1: Vec<i16> = vec![0, 4, 3, 0, 1, 0, 2, 0, 1, 0];
+        let height_map_v2: Vec<i16> = vec![0, 4, 3, 0, 1, 0, 2, 0, 1, 0];
+        let output_v1 = trap_water_v1(height_map_v1);
+        let output_v2 = trap_water_v2(height_map_v2);
+        assert_eq!(output_v1, 6);
+        assert_eq!(output_v2, 6);
     }
 }
